795eb91037a4823a58450d1a193afc2c0ea1f57b
Tiago Faria <tcfaria@gmail.com>
2019-01-12 22:08:55 -0500

    fix issue with wizard upload

--- kornell-gwt/src/main/webapp/angular/knl/modules/wizard/wizard.js
+++ kornell-gwt/src/main/webapp/angular/knl/modules/wizard/wizard.js
@@ -987,7 +987,12 @@ app.controller('FileController', [
           uploader.queue[0].url = uploader.requestUploadPath;
           uploader.queue[0].uploader.url = uploader.requestUploadPath;
           uploader.queue[0].method = 'PUT';
-          uploader.queue[0].fullURL = uploader.requestUploadPath.split('.s3.amazonaws.com')[1].split('?AWS')[0];
+          var splitRequestUploadPath = uploader.requestUploadPath.split('.s3.amazonaws.com');
+          if(splitRequestUploadPath.length > 1){
+              uploader.queue[0].fullURL = splitRequestUploadPath[1].split('?AWS')[0];
+          } else {
+              uploader.queue[0].fullURL = uploader.requestUploadPath;
+          }
           uploader.queue[0].fileUUID = $scope.fileUUID;
         }
       };
